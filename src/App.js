d26c783099b0801c7a4d3ffa369ad33a2e00c25b
euzhengxi <euzhengxi110@gmail.com>
2023-04-07 01:33:15 +0800

    test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Typewriter from 'typewriter-effect';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
