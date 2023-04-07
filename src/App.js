f43f6865f645353cac719324dbfc8d20405cbe8c
thebadone231 <chenhaoli123@gmail.com>
2023-04-07 11:55:08 +0800

    incorporated scoring and score history functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Text } from 'react';
 import SpeechRecognition, {
   useSpeechRecognition,
 } from 'react-speech-recognition';
@@ -9,6 +9,10 @@ import chalkboard from './assets/chalkboard.jpg';
 
 const Dictaphone = () => {
   const [micOn, setMicOn] = useState(false);
+  const [score, setScore] = useState(false);
+  const [performance, setPerformance] = useState(false);
+  const [history, setHistory] = useState([]);
+
   const {
     transcript,
     listening,
@@ -30,12 +34,65 @@ const Dictaphone = () => {
     SpeechRecognition.stopListening();
     setMicOn(false);
     console.log('mic is off');
+    scoring(
+      transcript,
+      "The thing that's great about this job is the time sourcing the items involves no traveling. I just look online to buy it."
+    );
   };
 
   const handleResetTranscript = () => {
     handleStopListening();
     resetTranscript();
+    setScore(false);
+    setPerformance(false);
   };
+
+  const scoring = (s1, s2) => {
+    // Convert both strings to lowercase
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    // Initialize the distance matrix
+    var d = [];
+    for (var i = 0; i <= s1.length; i++) {
+      d[i] = [];
+      d[i][0] = i;
+    }
+    for (var j = 0; j <= s2.length; j++) {
+      d[0][j] = j;
+    }
+
+    // Fill in the rest of the matrix
+    for (var i = 1; i <= s1.length; i++) {
+      for (var j = 1; j <= s2.length; j++) {
+        if (s1[i - 1] == s2[j - 1]) {
+          d[i][j] = d[i - 1][j - 1];
+        } else {
+          d[i][j] = Math.min(
+            d[i - 1][j] + 1, // Deletion
+            d[i][j - 1] + 1, // Insertion
+            d[i - 1][j - 1] + 1 // Substitution
+          );
+        }
+      }
+    }
+
+    // Return the Levenshtein distance
+    let score = Math.round(
+      (1 - d[s1.length][s2.length] / Math.max(s1.length, s2.length)) * 100
+    );
+    setScore(score);
+    setPerformance(score + '/100');
+  };
+
+  function updateScore(score) {
+    if (score) {
+      setHistory((prevHistory) => [...prevHistory, score]);
+    }
+  }
+  const displayhistory = history.map((value) => {
+    return <p>{value}</p>;
+  });
 
   return (
     <div style={{ height: '100vh', backgroundSize: '100% 100%' }}>
@@ -79,7 +136,7 @@ const Dictaphone = () => {
                   }}
                   cursorColor="white"
                   multiText={[
-                    "The thing that's great about this job is the time sourcing the items involves no traveling. I just look online to buy it. It's really as simple as that. While everyone else is searching for what they can sell, I sit in front of my computer and buy better stuff for less money and spend a fraction of the time doing it.",
+                    "The thing that's great about this job is the time sourcing the items involves no traveling. I just look online to buy it.",
                   ]}
                   typeSpeed={50}
                 />
@@ -110,6 +167,17 @@ const Dictaphone = () => {
               </div>
             </div>
             <p>{transcript}</p>
+            <br></br>
+            <p>Score: {score ? performance : '-'}</p>
+            <div>
+              {history.length > 0 ? <p>Score History</p> : <p></p>}
+              {history.map((item, index) => (
+                <p key={index}>{item}</p>
+              ))}
+              <button style={{}} onClick={() => updateScore(score)}>
+                Update Score
+              </button>
+            </div>
           </div>
         </ParallaxLayer>
       </Parallax>
